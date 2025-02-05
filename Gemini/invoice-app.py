bd67f2d36dbc5d2d9181a78829936cdafa8b3a22
Praneeth Kruthiventi <100612144+neeth97@users.noreply.github.com>
2025-02-04 23:08:40 -0700

    Update invoice-app.py

--- Gemini/invoice-app.py
+++ Gemini/invoice-app.py
@@ -1,73 +1,59 @@
-# Q&A Chatbot
-#from langchain.llms import OpenAI
-
-from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
-
 import streamlit as st
+from dotenv import load_dotenv
 import os
-import pathlib
-import textwrap
 from PIL import Image
-
-
 import google.generativeai as genai
+import pdfplumber
+import io
 
+#load environment varibles
+load_dotenv()
+#configuration google gemini
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model=genai.GenerativeModel('gemini-1.5-flash')
 
-## Function to load OpenAI model and get respones
-
-def get_gemini_response(input,image,prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input,image[0],prompt])
-    return response.text
-    
-
-def input_image_setup(uploaded_file):
-    # Check if a file has been uploaded
-    if uploaded_file is not None:
-        # Read the file into bytes
-        bytes_data = uploaded_file.getvalue()
-
-        image_parts = [
-            {
-                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
-                "data": bytes_data
-            }
-        ]
-        return image_parts
+def get_gemini_response(input_text,image=None):
+    if image is None:
+        rsponse=model.generate_content(input_text)
     else:
-        raise FileNotFoundError("No file uploaded")
+        response=model.generate_content([input_text,image])
+    return response.text
 
+st.title('Invoice Extraxtion using Google Gemini')
 
-##initialize our streamlit app
+st.write('Upload an invoice (image or PDF) and ask questions about the extraxted content.')
 
-st.set_page_config(page_title="Gemini Image Demo")
+input_text=st.text_input('Enter your query.')
 
-st.header("Gemini Application")
-input=st.text_input("Input Prompt: ",key="input")
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-image=""   
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+upload_file=st.file_uploader('Upload an invoice (Image/PDf)',type=['jpg','jpeg','pdf'])
 
+image=None
+text_from_pdf=None
 
-submit=st.button("Tell me about the image")
+if upload_file is not None:
+    if upload_file.type in ['image/jpeg','image/jpg']:
+        image=Image.open(upload_file)
+        st.image(image,caption='uploaded Image',use_column_width=True)
 
-input_prompt = """
-               You are an expert in understanding invoices.
-               You will receive input images as invoices &
-               you will have to answer questions based on the input image
-               """
+    elif upload_file.type == 'application/pdf':
+        with pdfplumber.open(io.BytesIO(upload_file.read())) as pdf:
+            text_from_pdf=''
+            for page in pdf.pages:
+                extracted_text=page.extract_text()
+                text_from_pdf+=extracted_text
+            st.write('Extracted text from PDF')
+            st.write(text_from_pdf)
 
-## If ask button is clicked
+input_prompt="you are an expert in understanding invoice. We will upload an invoice (Image or PDF) and you will answer questions base on the uploaded content."       
 
-if submit:
-    image_data = input_image_setup(uploaded_file)
-    response=get_gemini_response(input_prompt,image_data,input)
-    st.subheader("The Response is")
-    st.write(response)
+if st.button('Extract Information'):
+    if image is not None :
+        respons= get_gemini_response(input_prompt + input_text,image)
+    elif text_from_pdf is not None:
+        respons=get_gemini_response(input_prompt+ input_text + text_from_pdf)
+    else :
+        respons="Please upload a valid image or PDF file."
+    st.write("Gemini's Response"
+             )
+    st.write(respons)

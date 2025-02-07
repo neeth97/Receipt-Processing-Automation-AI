ac91b4234bceab8688eefeccc8453cff81668286
Praneeth Kruthiventi <100612144+neeth97@users.noreply.github.com>
2025-02-06 18:28:43 -0700

    Update invoice-app.py

--- Gemini/invoice-app.py
+++ Gemini/invoice-app.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # Configure API Key
 # os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key="AIzaSyBJkIPuOecs35-QINMFkXtZg0TyyQBMU8I")
+genai.configure(api_key="AIzaSyAWxoMHLIW3D1w4JHGQUVUBTywfGhLCCYk")
 
 # Function to get AI response
 def get_gemini_response(image):
